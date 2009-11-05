dcec1afdadf28a9d42d56d59d302662058434cdc
Jon Leighton <j@jonathanleighton.com>
2009-11-05 11:41:09 +0000

    Add a stack and scoping. Now class definition, method definition and method dispatch works, to some limited extent

--- carat.rb
+++ carat.rb
@@ -19,79 +19,238 @@ module Carat
   end
   
   class Runtime
-    class << self
-      def eval(node_type, &block)
-        define_method("eval_#{node_type}", &block)
+    class Object
+      attr_reader :klass, :instance_variables
+      
+      def initialize(klass)
+        @klass = klass
+        @instance_variables = {}
+      end
+      
+      def lookup_method(method_name)
+        klass.methods[method_name]
+      end
+      
+      def to_s
+        "<object:#{klass}>"
+      end
+    end
+  
+    class Class < Object
+      attr_reader :methods, :name, :superclass
+      
+      def initialize(name, superclass)
+        super(nil) # TODO: Should be reference to Class object in the object language
+        @name, @superclass = name, superclass
+        @methods = {}
+      end
+      
+      def to_s
+        "<class:#{name}>"
+      end
+    end
+    
+    class Method
+      attr_reader :args, :contents
+      
+      def initialize(args, contents)
+        @args, @contents = args, contents
+      end
+    end
+    
+    # The top level scope takes care of "globals". In this context "globals" can either be constants
+    # (which are globally available) or actual global variables.
+    class TopLevelScope
+      attr_reader :symbols, :globals
+      
+      def initialize(self_object)
+        @symbols = { :self => self_object }
+        @globals = {}
+      end
+      
+      def []=(symbol, value)
+        symbols[symbol] = value
+      end
+      
+      def [](symbol)
+        symbols[symbol] || raise(RuntimeError, "local variable '#{symbol}' is undefined")
+      end
+      
+      # TODO: Make this work
+      # For now:
+      alias_method :constants, :globals
+      alias_method :global_variables, :globals
+=begin
+      def constants[]=(symbol, value)
+        globals[symbol] = value
+      end
+      
+      def constants[](symbol)
+        globals[symbol] || raise(RuntimeError, "constant '#{symbol}' is undefined")
+      end
+      
+      def global_variables[]=(symbol, value)
+        globals[symbol] = value
+      end
+      
+      def global_variables[](symbol)
+        globals[symbol] || raise(RuntimeError, "global variable '#{symbol}' is undefined")
+      end
+=end
+    end
+    
+    # Standard Scopes have a parent, and they use the hierarchy to lookup globals
+    class Scope < TopLevelScope
+      attr_reader :parent
+      
+      def initialize(self_object, parent)
+        super(self_object)
+        @parent = parent
+      end
+      
+      def globals
+        parent.globals
+      end
+      
+      def [](symbol)
+        symbols[symbol] || parent[symbol]
+      end
+    end
+    
+    class Stack < Array
+      # Accept a frame onto the stack, evaluate it, then remove it
+      def <<(frame)
+        super(frame)
+        result = frame.eval(self)
+        pop
+        result
+      end
+    end
+    
+    # Evaluates a sexp within the given scope. This is where all the evaluation logic lives.
+    class Frame
+      attr_reader :sexp, :scope
+      
+      class << self
+        def eval(node_type, &block)
+          define_method("eval_#{node_type}", &block)
+        end
+      end
+      
+      def initialize(sexp, scope)
+        @sexp, @scope = sexp, scope
+      end
+      
+      def sexp_type
+        sexp.first
+      end
+      
+      def sexp_body
+        sexp[1..-1]
+      end
+      
+      def error(message)
+        raise RuntimeError, message
+      end
+      
+      # Theoretically a frame can exist independently of a stack, but when we are evaluating the
+      # frame we need a reference to the stack so that we can add new frames to it.
+      def eval(stack)
+        @stack = stack
+        if respond_to?("eval_#{sexp_type}")
+          send("eval_#{sexp_type}", *sexp_body) unless sexp_body.empty?
+        else
+          error "'#{sexp_type}' not implemented"
+        end
+      ensure
+        @stack = nil
+      end
+      
+      # Creates a new frame for the sexp and scope, and adds it to the current stack. If scope is
+      # not given, it will default to the current scope.
+      def stack_eval(sexp, scope = nil)
+        @stack << Frame.new(sexp, scope || self.scope)
+      end
+      
+      eval(:false) { false }
+      eval(:true) { true }
+      eval(:nil) { nil }
+      
+      # A literal number value. This is evaluated by the lexer, so we can just use it straight off.
+      eval :lit do |value|
+        value
+      end
+      
+      # A block of statements. Evaluate each in turn and return the result of the last one.
+      eval :block do |*statements|
+        statements.reduce(nil) { |last_result, statement| stack_eval(statement) }
+      end
+      
+      # Make an assignment
+      eval :lasgn do |identifier, value|
+        scope[identifier] = stack_eval(value)
+      end
+      
+      # Get a variable
+      eval :lvar do |identifier|
+        scope[identifier]
+      end
+      
+      # call a method or retrieve a local variable. Unfortunately the sexp does not differentiate
+      # between "foo" and "foo()". There is a difference - the second can only be a method call, but
+      # the first might be a method call or a local variable lookup.
+      eval :call do |receiver, method_name, args|
+        if receiver.nil? && stack_eval(args).nil?
+          value = scope[method_name]
+          if false # should be "if value is a method"
+            raise RuntimeError, "not implemented"
+          else
+            value
+          end
+        else
+          object = stack_eval(receiver)
+          
+          if method_name == :new
+            # Temporary special case
+            Object.new(object)
+          else
+            stack_eval(object.lookup_method(method_name).contents, Scope.new(object, scope))
+          end
+        end
+      end
+      
+      eval :arglist do |*identifiers|
+        identifiers
+      end
+      
+      eval :class do |class_name, superclass, contents|
+        klass = Class.new(class_name, superclass)
+        scope.constants[class_name] = klass
+        stack_eval(contents, Scope.new(klass, scope))
+      end
+      
+      eval :scope do |statement|
+        stack_eval(statement)
+      end
+      
+      eval :defn do |method_name, args, contents|
+        scope[:self].methods[method_name] = Method.new(args, contents)
+        nil
+      end
+      
+      eval :const do |const_name|
+        scope.constants[const_name]
       end
     end
     
     def initialize
-      @environment = {}
-    end
-    
-    def set(identifier, value)
-      @environment[identifier] = value
-    end
-    
-    def get(identifier)
-      @environment[identifier] || raise(RuntimeError, "'#{identifier}' is undefined")
-    end
-    
-    def error(message)
-      raise RuntimeError, message
+      @scope = TopLevelScope.new(nil)
     end
     
     def eval(sexp)
-      node_type, body = sexp.first, sexp[1..-1]
-      if respond_to?("eval_#{node_type}")
-        send("eval_#{node_type}", *body)
-      else
-        error "'#{node_type}' not implemented"
-      end
-    end
-    
-    eval(:false) { false }
-    eval(:true) { true }
-    eval(:nil) { nil }
-    
-    # A literal number value. This is evaluated by the lexer, so we can just use it straight off.
-    eval :lit do |value|
-      value
-    end
-    
-    # A block of statements. Evaluate each in turn and return the result of the last one.
-    eval :block do |*statements|
-      statements.reduce(nil) { |last_result, statement| eval(statement) }
-    end
-    
-    # Make an assignment
-    eval :lasgn do |identifier, value|
-      set(identifier, eval(value))
-    end
-    
-    # Get a variable
-    eval :lvar do |identifier|
-      get(identifier)
-    end
-    
-    # call a method or retrieve a local variable. Unfortunately the sexp does not differentiate
-    # between "foo" and "foo()". There is a difference - the second can only be a method call, but
-    # the first might be a method call or a local variable lookup.
-    eval :call do |receiver, identifier, args|
-      if receiver.nil? && eval(args).empty?
-        value = get(identifier)
-        if false # should be "if value is a method"
-          # not implemented - evaluate the method
-        else
-          value
-        end
-      else
-        # not implemented - get and evaluate the method
-      end
-    end
-    
-    eval :arglist do |*identifiers|
-      identifiers
+      @stack = Stack.new
+      @frame = Frame.new(sexp, @scope)
+      @stack << @frame
     end
   end
 end
